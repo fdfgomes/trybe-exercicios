1dac67bdbda9f1557b85452bff48272d84bc11a0
Felipe Gomes <fdefariasgomes@gmail.com>
2022-10-17 20:15:47 -0300

    resolução do segundo exercício: criar função que retorna a maior palavra contida em uma frase

--- fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios-parte-2.js
+++ fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios-parte-2.js
@@ -1,12 +1,28 @@
 // exercício 1: criar uma função que recebe um número e retorna seu fatorial
-const factorial = number => {
+const factorial = (number) => {
   let result = 1;
-  let factorialString = ''
-  for(let x = number; x >= 1; x -= 1) {
-    factorialString += `${x}${x > 1 ? ' * ' : ''}`;
+  let factorialString = "";
+  for (let x = number; x >= 1; x -= 1) {
+    factorialString += `${x}${x > 1 ? " * " : ""}`;
     result = result * x;
   }
   return `${number}! = ${factorialString} = ${result}`;
-}
+};
 
 console.log(`Esse é o fatorial ${factorial(4)}`);
+
+// exercício 2: criar uma função que recebe um frase como parâmetro, e retorna a maior palavra contida nela
+const longestWord = (string) => {
+  const splittedString = string.split(" ");
+  let longestWordIndex = 0;
+  for (let x = 0; x < splittedString.length; x += 1) {
+    if (splittedString[x].length > splittedString[longestWordIndex].length) {
+      longestWordIndex = x;
+    }
+  }
+  return splittedString[longestWordIndex];
+};
+
+console.log(
+  longestWord("Antônio foi ao banheiro e não sabemos o que aconteceu")
+); // retorna 'aconteceu'
