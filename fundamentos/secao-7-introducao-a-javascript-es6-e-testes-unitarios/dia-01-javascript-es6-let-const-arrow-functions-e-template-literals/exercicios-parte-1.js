c81e626c9e09a5a0fb1aa273054114ab99f606c2
Felipe Gomes <fdefariasgomes@gmail.com>
2022-10-17 19:40:45 -0300

    resolução do primeiro exercício: refatorar função utilizando arrow function, template literals e ternary operator

--- fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios-parte-1.js
+++ fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios-parte-1.js
@@ -1,13 +1,18 @@
-function testingScope(escopo) {
-  if (escopo === true) {
-    var ifScope = "Não devo ser utilizada fora do meu escopo (if)";
-    ifScope = ifScope + " ótimo, fui utilizada no escopo !";
-    console.log(ifScope);
-  } else {
-    var elseScope = "Não devo ser utilizada fora do meu escopo (else)";
-    console.log(elseScope);
-  }
-  console.log(ifScope + " o que estou fazendo aqui ? :O"); // Se necessário esta linha pode ser removida.
-}
+// function testingScope(escopo) {
+//   if (escopo === true) {
+//     var ifScope = "Não devo ser utilizada fora do meu escopo (if)";
+//     ifScope = ifScope + " ótimo, fui utilizada no escopo !";
+//     console.log(ifScope);
+//   } else {
+//     var elseScope = "Não devo ser utilizada fora do meu escopo (else)";
+//     console.log(elseScope);
+//   }
+//   console.log(ifScope + " o que estou fazendo aqui ? :O"); // Se necessário esta linha pode ser removida.
+// }
 
-testingScope(true);
+const testingScope = escopo =>
+  escopo
+    ? `Não devo ser utilizada fora do meu escopo (if) ótimo, fui utilizada no escopo !`
+    : `Não devo ser utilizada fora do meu escopo (else) o que estou fazendo aqui ? :O`;
+
+console.log(testingScope(true));
