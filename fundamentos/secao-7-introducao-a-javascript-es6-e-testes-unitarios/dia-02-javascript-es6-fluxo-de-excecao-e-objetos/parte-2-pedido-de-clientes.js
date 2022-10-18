478bfb543f887ea82cd25a219e0aaa8de82f7148
Felipe Gomes <fdefariasgomes@gmail.com>
2022-10-18 19:30:59 -0300

    completando a função orderModifier()

--- fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte-2-pedido-de-clientes.js
+++ fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte-2-pedido-de-clientes.js
@@ -41,10 +41,42 @@ const customerInfo = (order) =>
     /rua/gi,
     "R."
   )}, Nº: ${order.address.number}, AP: ${order.address.apartment}`;
+
 console.log(customerInfo(order));
 
 const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
+  // clonar o objeto order, e realizar as modificações no objeto clonado
+  // preservar o original
+  const newOrderObject = Object.assign({}, order);
+  // trocar nome do cliente
+  newOrderObject.name = "Luiz Silva";
+  // trocar valor total do pedido
+  newOrderObject.payment.total = 50;
+  // criar variável com a resposta que a função retornará
+  let response = `Olá ${newOrderObject.name}, o total do seu pedido de `;
+
+  // colocar sabor das pizzas na resposta
+  for (let x = 0; x < Object.keys(newOrderObject.order.pizza).length; x += 1) {
+    response += `${Object.keys(newOrderObject.order.pizza)[x]}${
+      x !== Object.keys(newOrderObject.order.pizza).length - 1 ? ", " : " "
+    }`;
+  }
+  // fazer verificação se existem bebidas no pedido
+  if (
+    Object.keys(newOrderObject.order).includes("drinks") &&
+    Object.values(newOrderObject.order.drinks.length > 0)
+  ) {
+    response += `e `;
+    // colocar bebidas na resposta
+    for (const drink of Object.values(newOrderObject.order.drinks)) {
+      response += drink.type;
+    }
+  }
+  response += `é R$ ${newOrderObject.payment.total
+    .toFixed(2)
+    .toString()
+    .replace(".", ",")}.`;
+  return response;
 };
 
-orderModifier(order);
+console.log(orderModifier(order));
