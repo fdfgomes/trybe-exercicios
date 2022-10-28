8a4809cd7bd482492c3fffda1e13ad78dbdfbf12
Felipe Gomes <fdefariasgomes@gmail.com>
2022-10-28 11:15:19 -0300

    corrigindo erro no exercício 4

--- fundamentos/secao-8-high-order-functions-do-javascript-es6/dia-04-javascript-es6-high-order-functions-reduce/exercicio4.js
+++ fundamentos/secao-8-high-order-functions-do-javascript-es6/dia-04-javascript-es6-high-order-functions-reduce/exercicio4.js
@@ -1,14 +1,12 @@
-const books = require('./books');
+// const books = require('./books');
 
 const longestNamedBook = (booksArray) =>
-  booksArray.reduce((acc, currValue) => {
-    console.log(acc, currValue);
-    if (currValue.name.length > acc.name.length) acc = currValue;
-    // currIndex === 0
-    //   ? (acc = currValue)
-    //   : currValue.name.length > acc.name.length && (acc = currValue),
-  }, booksArray[0]);
+  booksArray.reduce(
+    (acc, currValue) =>
+      currValue.name.length > acc.name.length ? currValue : acc,
+    booksArray[0]
+  );
 
-console.log(longestNamedBook(books));
+// console.log(longestNamedBook(books));
 
 module.exports = longestNamedBook;
